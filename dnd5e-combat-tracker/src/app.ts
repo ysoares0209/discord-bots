8f1fc7033e0e2461b23de55f31356ed3b8133d0d
ysoares0209 <yan@westpoint.io>
2022-08-20 01:04:57 -0300

    initial commit

---
+++ dnd5e-combat-tracker/src/app.ts
@@ -0,0 +1,9 @@
+import "dotenv/config";
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
