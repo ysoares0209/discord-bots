5f81f7b4b4de87e0c3cd4711a341bd6456cd570d
ysoares0209 <yan@westpoint.io>
2022-08-20 23:12:40 -0300

    update command type to be a constant

--- dnd5e-combat-tracker/src/availableCommands.ts
+++ dnd5e-combat-tracker/src/availableCommands.ts
@@ -4,10 +4,12 @@ export interface Command {
   type: number;
 }
 
+const chartInputCommandType = 1;
+
 export const TEST: Command = {
   name: "test",
   description: "Returns hello world",
-  type: 1,
+  type: chartInputCommandType,
 };
 
 export const ALL_COMMANDS: Array<Command> = [TEST];
