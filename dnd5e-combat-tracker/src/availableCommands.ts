820ec70571998ce0098919b847d682157fbdccd0
ysoares0209 <yan@westpoint.io>
2022-08-21 16:32:43 -0300

    add ADD_CHARACTER command definition

--- dnd5e-combat-tracker/src/availableCommands.ts
+++ dnd5e-combat-tracker/src/availableCommands.ts
@@ -2,6 +2,12 @@ export interface Command {
   name: string;
   description: string;
   type: 1 | 2 | 3;
+  options?: {
+    name: string;
+    description: string;
+    type: number;
+    required: boolean;
+  }[];
 }
 
 const chartInputCommandType = 1;
@@ -19,4 +25,28 @@ export const START_TRACKER: Command = {
   type: chartInputCommandType,
 };
 
-export const ALL_COMMANDS: Array<Command> = [TEST, START_TRACKER];
+export const ADD_CHARACTER: Command = {
+  name: "add-character",
+  description: "Adds a character to the current tracker",
+  type: chartInputCommandType,
+  options: [
+    {
+      name: "character-name",
+      description: "the name the character should have in the tracker",
+      type: 3,
+      required: true,
+    },
+    {
+      name: "character-initiative",
+      description: "the initiave value for this character",
+      type: 3,
+      required: true,
+    },
+  ],
+};
+
+export const ALL_COMMANDS: Array<Command> = [
+  TEST,
+  START_TRACKER,
+  ADD_CHARACTER,
+];
