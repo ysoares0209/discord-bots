040c9d7d35dcfb906a8bab09f89aa7b8b8627c1e
ysoares0209 <yan@westpoint.io>
2022-08-20 23:32:03 -0300

    fix loadGuildCommands function

--- dnd5e-combat-tracker/src/utils/loadGuildCommands.ts
+++ dnd5e-combat-tracker/src/utils/loadGuildCommands.ts
@@ -7,6 +7,18 @@ interface LoadGuildCommands {
   commands: Array<Command>;
 }
 
+interface GuildCommand {
+  id: string;
+  application_id: string;
+  version: string;
+  default_permission: boolean;
+  default_member_permissions: unknown;
+  type: number;
+  name: string;
+  description: string;
+  guild_id: string;
+}
+
 const loadGuildCommands = async ({
   appId,
   guildId,
@@ -17,24 +29,29 @@ const loadGuildCommands = async ({
       throw Error("Could not find required envs!");
     }
     const endpoint = `applications/${appId}/guilds/${guildId}/commands`;
-    for await (const command of commands) {
-      const result = await buildDiscordRequest({
-        endpoint,
-        options: { method: "GET" },
-      });
-      const data = (await result.json()) as Array<unknown>;
-      console.log(data);
-      if (data.length) {
-        console.log(`Command ${command.name} already installed... proceed`);
-        continue;
-      }
-      console.log(`Command ${command.name} is not installed... installing`);
+    const allGuildCommands = (await buildDiscordRequest({
+      endpoint,
+      options: { method: "GET" },
+    }).then((result) => result.json())) as Array<GuildCommand>;
+    const installedCommands = allGuildCommands.map(
+      (guildCommands) => guildCommands.name
+    );
+    const commandsToInstall = commands.filter(
+      (command) => !installedCommands.includes(command.name)
+    );
+    console.log(`installed commands: ${installedCommands.join(",")}`);
+    console.log(
+      `commands to install: ${commandsToInstall
+        .map((command) => command.name)
+        .join(",")}`
+    );
+    for await (const command of commandsToInstall) {
       await buildDiscordRequest({
         endpoint,
         options: { method: "POST", body: JSON.stringify(command) },
       });
-      console.log("Command installed!");
     }
+    console.log("All commands installed!");
   } catch (error) {
     console.log(error);
     return { error: (error as Error).message };
